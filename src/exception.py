42eb3bedf7c30d8ffa86298d547b4fac2255a1dc
ravina <vermaravina029@gmail.com>
2023-10-02 18:00:58 +0300

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -2,6 +2,7 @@
 #here we will write our own custom exceptions.
 import sys
 import logging 
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -17,7 +18,9 @@ class CustomException(Exception):
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
 
     def __str__(self):
-        return self.error_message    
+        return self.error_message 
+
+     
 
 
 
